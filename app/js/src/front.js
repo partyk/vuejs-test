79107820d7f75f5cc1e8a78163918ed370125664
Bohdan Partyk <info@podebrady.org>
2018-09-16 11:52:27 +0200

    Lesson 3 - Conditional Rendering

--- app/js/src/front.js
+++ app/js/src/front.js
@@ -6,6 +6,8 @@ global.app = new Vue({
     data: {
         product: 'Socks',
         image: '/ui/images/vmSocks-green-onWhite.jpg',
-        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+        inStock: true,
+        onSale: true
     }
 });
