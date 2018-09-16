e74063ab3c35e121969a7f25ae7c184bfd856b2c
Bohdan Partyk <info@podebrady.org>
2018-09-16 11:27:40 +0200

    Attribute Binding

--- app/js/src/front.js
+++ app/js/src/front.js
@@ -5,6 +5,7 @@ global.app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'A pair of warm fuzzy socks'
+        image: '/ui/images/vmSocks-green-onWhite.jpg',
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
     }
 });
