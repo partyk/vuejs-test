6b4dceb4d150b308932a02e0cf4d94f9cfdae43e
Bohdan Partyk <info@podebrady.org>
2018-09-16 11:06:10 +0200

    The Vue Instance

--- app/js/src/front.js
+++ app/js/src/front.js
@@ -1,14 +1,10 @@
 import 'babel-polyfill'; // polyfill
 import Vue from 'vue'; // vue
-import App from '.././components/ui/App';
-
-global.Vue = Vue;
-global.jQuery = jQuery;
 
 global.app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
-    },
-    render: h => h(App)
+        product: 'Socks',
+        description: 'A pair of warm fuzzy socks'
+    }
 });
