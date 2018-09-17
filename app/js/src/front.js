355d5478a019620957f4c8aef746538d3db41b38
Bohdan Partyk <info@podebrady.org>
2018-09-17 06:50:57 +0200

    Lesson 7 - Computed Properties

--- app/js/src/front.js
+++ app/js/src/front.js
@@ -4,26 +4,44 @@ import Vue from 'vue'; // vue
 global.app = new Vue({
     el: '#app',
     data: {
+        selectedVariant: 0,
         product: 'Socks',
         brand: 'Vue Mastery',
-        image: '/ui/images/vmSocks-green-onWhite.jpg',
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
-                variantImage: '/ui/images/vmSocks-green-onWhite.jpg'
+                variantImage: '/ui/images/vmSocks-green-onWhite.jpg',
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
-                variantImage: '/ui/images/vmSocks-blue-onWhite.jpg'
+                variantImage: '/ui/images/vmSocks-blue-onWhite.jpg',
+                variantQuantity: 0
             }
         ],
         cart: 0
         // sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' are on sale!';
+            }
+            return this.brand + ' ' + this.product + ' are not on sale';
+        }
     },
     methods: {
         addToCart() {
@@ -32,8 +50,8 @@ global.app = new Vue({
         removeToCart() {
             this.cart = this.cart > 0 ? this.cart - 1 : 0;
         },
-        updateProduct(variantImage) {
-            this.image = variantImage;
+        updateProduct(index) {
+            this.selectedVariant = index;
         }
     }
 });
