0997252ed6432e85753bb71c0e758a679a2745e0
Bohdan Partyk <info@podebrady.org>
2018-09-21 13:01:32 +0200

    Lesson 7 - Computed Properties

--- app/js/src/front.js
+++ app/js/src/front.js
@@ -1,30 +1,83 @@
 import 'babel-polyfill'; // polyfill
 import Vue from 'vue'; // vue
 
-global.app = new Vue({
-    el: '#app',
-    data: {
-        selectedVariant: 0,
-        product: 'Socks',
-        brand: 'Vue Mastery',
-        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: '/ui/images/vmSocks-green-onWhite.jpg',
-                variantQuantity: 10
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: '/ui/images/vmSocks-blue-onWhite.jpg',
-                variantQuantity: 0
-            }
-        ],
-        cart: 0
-        // sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+Vue.component('product', {
+    template: `
+        <div class="product">
+
+            <div class="product-image">
+                <img :src="image">
+            </div>
+
+            <div class="product-info">
+                <h1>{{ title }}</h1>
+                <p v-if="inStock">In Stock</p>
+                <p
+                    v-else
+                    v-bind:class="{ outOfStock: !inStock }"
+                    >Out of Stock</p>
+                <p>{{ sale }}</p>
+
+                <ul>
+                    <li v-for="detail in details">{{ detail }}</li>
+                </ul>
+
+                <!--<ul>-->
+                    <!--<li v-for="size in sizes">{{ size }}</li>-->
+                <!--</ul>-->
+
+                <div
+                    class="color-box"
+                    v-for="(variant, index) in variants"
+                    :key="variant.variantId"
+                    :data-id="variant.variantId"
+                    :style="{ backgroundColor: variant.variantColor}"
+                    @mouseover="updateProduct(index)"
+                    >
+                    <!--<p>-->
+                        <!--{{ variant.variantColor }}-->
+                    <!--</p>-->
+                </div>
+
+                <div class="cart">
+                    <p>Cart({{ cart }})</p>
+                </div>
+
+                <button
+                    v-on:click="addToCart"
+                    :disabled="!inStock"
+                    :class="{ disabledButton: !inStock}"
+                >Add to cart</button>
+                <button v-on:click="removeToCart" v-show="this.cart > 0">Remove to cart</button>
+
+
+            </div>
+
+        </div>
+    `,
+    data() {
+        return {
+            selectedVariant: 0,
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: '/ui/images/vmSocks-green-onWhite.jpg',
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: '/ui/images/vmSocks-blue-onWhite.jpg',
+                    variantQuantity: 0
+                }
+            ],
+            cart: 0
+        };
     },
     computed: {
         title() {
@@ -53,5 +106,12 @@ global.app = new Vue({
         updateProduct(index) {
             this.selectedVariant = index;
         }
+    }
+});
+
+global.app = new Vue({
+    el: '#app',
+    data: {
+        premium: true
     }
 });
